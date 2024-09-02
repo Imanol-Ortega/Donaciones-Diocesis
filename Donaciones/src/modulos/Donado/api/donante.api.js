b0c676fef9475094b1b09105177d161a5bcfff43
Imanol-Ortega <imanicoortemohr018@gmail.com>
2024-09-01 23:15:48 -0400

    configurando para deploy en vercel

--- Donaciones/src/modulos/Donado/api/donante.api.js
+++ Donaciones/src/modulos/Donado/api/donante.api.js
@@ -1,13 +1,13 @@
 import axios from "axios"
 
 export const guardarDonanteRequest = async(values)=>{
-    return await axios.post('http://localhost:3000/donante/guardar',values);
+    return await axios.post(`${import.meta.env.VITE_BACKEND_URL}/donante/guardar`,values);
 };
 
 export const obtenerDonantesRequest = async()=>{
-    return await axios.get('http://localhost:3000/donante/obtener');
+    return await axios.get(`${import.meta.env.VITE_BACKEND_URL}/donante/obtener`);
 };
 
 export const obtenerUnaDonante = async(id)=>{
-    return await axios.get(`http://localhost:3000/donante/obtener/${id}`);
+    return await axios.get(`${import.meta.env.VITE_BACKEND_URL}/donante/obtener/${id}`);
 }
