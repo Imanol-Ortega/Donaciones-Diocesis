1dc95fea81e101d8cebfbc93ac6fce68bdcb40d3
Imanol-Ortega <imanicoortemohr018@gmail.com>
2024-09-11 06:35:16 -0400

    comenzando cambios

--- Donaciones/src/modulos/Donante/formulario/FormDonante.jsx
+++ Donaciones/src/modulos/Donante/formulario/FormDonante.jsx
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { guardarDonanteRequest } from "../api/donante.api";
+import Diocesis from "../../../static/diocesis.png";
 
 function FormDonante() {
     const {
@@ -41,21 +42,28 @@ function FormDonante() {
                 <div className="container mx-auto h-full flex flex-1 justify-center items-center">
                     <div className="w-full max-w-lg">
                         <div className="leading-loose">
+                            <div className="max-w-full flex justify-center align-middle p-2">
+                                <img
+                                    src={Diocesis}
+                                    alt="img"
+                                    className="w-48 h-48 rounded-full object-cover"
+                                />
+                            </div>
                             <form
                                 onSubmit={submit}
-                                className="max-w-full p-10 bg-white rounded shadow-xl flex justify-center flex-col font-thin"
+                                className="max-w-full p-10 bg-[#263a21] rounded shadow-xl flex justify-center flex-col font-thin"
                             >
                                 <div className="flex justify-start">
-                                    <p className="text-black text-center text-xl font-bold border-b-2 border-blue-600 w-full py-2">
-                                        Donacion
+                                    <p className="text-white text-center text-xl font-bold border-b-2 border-[#263a21] w-full py-2">
+                                        Donación
                                     </p>
                                 </div>
 
-                                <label className="block text-base text-black mt-2 font-semibold">
+                                <label className="block text-base text-white mt-2 font-semibold">
                                     Nombre
                                 </label>
                                 <input
-                                    className="w-full px-5 py-1 text-gray-900 bg-gray-200 rounded focus:outline-none focus:bg-gray-100"
+                                    className="w-full px-5 py-1 text-gray-900 bg-gray-200 rounded focus:outline-none focus:bg-gray-100 placeholder-slate-600"
                                     type="text"
                                     placeholder="Escriba su nombre"
                                     {...register("nombre", { required: true })}
@@ -65,11 +73,11 @@ function FormDonante() {
                                         El campo esta vacío
                                     </div>
                                 )}
-                                <label className="block text-base text-black mt-2 font-semibold">
+                                <label className="block text-base text-white mt-2 font-semibold">
                                     Nro. de Teléfono
                                 </label>
                                 <input
-                                    className="w-full px-5 py-1 text-gray-900 bg-gray-200 rounded focus:outline-none focus:bg-gray-100"
+                                    className="w-full px-5 py-1 text-gray-900 bg-gray-200 rounded focus:outline-none focus:bg-gray-100 placeholder-slate-600"
                                     type="text"
                                     placeholder="Escriba su nro de teléfono"
                                     {...register("telefono", {
@@ -81,11 +89,11 @@ function FormDonante() {
                                         El campo esta vacío
                                     </div>
                                 )}
-                                <label className="block text-base text-black mt-2 font-semibold">
+                                <label className="block text-base text-white mt-2 font-semibold">
                                     Dirección
                                 </label>
                                 <textarea
-                                    className="w-full px-5 py-1 text-gray-900 bg-gray-200 rounded focus:outline-none focus:bg-gray-100 resize-none"
+                                    className="w-full px-5 py-1 text-gray-900 bg-gray-200 rounded focus:outline-none focus:bg-gray-100 resize-none placeholder-slate-600"
                                     placeholder="Escriba su lugar de residencia o de donde buscar"
                                     {...register("direccion", {
                                         required: true,
@@ -98,11 +106,11 @@ function FormDonante() {
                                     </div>
                                 )}
 
-                                <label className="block text-base text-black mt-2 font-semibold">
+                                <label className="block text-base text-white mt-2 font-semibold">
                                     Donación
                                 </label>
                                 <textarea
-                                    className="w-full px-5 py-1 text-gray-900 bg-gray-200 rounded focus:outline-none focus:bg-gray-100 resize-none"
+                                    className="w-full px-5 py-1 text-gray-900 bg-gray-200 rounded focus:outline-none focus:bg-gray-100 resize-none placeholder-slate-600"
                                     placeholder="Escriba lo que va a donar"
                                     {...register("donacion", {
                                         required: true,
@@ -114,11 +122,11 @@ function FormDonante() {
                                         El campo esta vacío
                                     </div>
                                 )}
-                                <label className="block text-base text-black mt-2 font-semibold">
+                                <label className="block text-base text-white mt-2 font-semibold">
                                     Observación
                                 </label>
                                 <textarea
-                                    className="w-full px-5 py-1 text-gray-900 bg-gray-200 rounded focus:outline-none focus:bg-gray-100 resize-none"
+                                    className="w-full px-5 py-1 text-gray-900 bg-gray-200 rounded focus:outline-none focus:bg-gray-100 resize-none placeholder-slate-600"
                                     placeholder="Escriba una observación"
                                     {...register("observacion", {
                                         required: true,
@@ -132,7 +140,7 @@ function FormDonante() {
                                 )}
                                 <div>
                                     <button
-                                        className="px-4 py-1 mt-4 text-white font-light tracking-wider bg-blue-800 hover:bg-blue-700 rounded"
+                                        className="px-4 py-1 mt-4 text-black font-thin tracking-wider bg-white hover:bg-slate-100 rounded"
                                         type="submit"
                                     >
                                         Guardar
