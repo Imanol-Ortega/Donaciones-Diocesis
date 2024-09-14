ad5daa0ca63c33dd01c2d7e1d6aeb27665176b08
Imanol-Ortega <imanicoortemohr018@gmail.com>
2024-09-13 20:41:50 -0400

    Se cambio el form de la donacion cambiando la direccion por un map

--- Donaciones/src/modulos/Donante/formulario/FormDonante.jsx
+++ Donaciones/src/modulos/Donante/formulario/FormDonante.jsx
@@ -5,6 +5,26 @@ import { useNavigate } from "react-router-dom";
 import { guardarDonanteRequest } from "../api/donante.api";
 import Diocesis from "../../../static/diocesis.png";
 
+import { useState } from "react";
+import {
+    Dialog,
+    Button,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+    IconButton,
+    Typography,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import {
+    MapContainer,
+    Marker,
+    Popup,
+    TileLayer,
+    useMapEvents,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 function FormDonante() {
     const {
         register,
@@ -15,9 +35,40 @@ function FormDonante() {
     } = useForm();
     const navigate = useNavigate();
 
+    const [open, setOpen] = useState(false);
+    const [position, setPosition] = useState(null);
+    const [flyPosition, setFlyPosition] = useState(null);
+    const [positionLoad, setPositionLoad] = useState([
+        -27.338697141418727, -55.86717871248513,
+    ]);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setFlyPosition(null);
+        setOpen(false);
+    };
+
+    const Markers = () => {
+        useMapEvents({
+            click: (e) => {
+                const newPosition = [e.latlng.lat, e.latlng.lng];
+                setValue("latitude", newPosition[0]);
+                setValue("longitude", newPosition[1]);
+                setPosition(newPosition);
+                setPositionLoad(newPosition);
+                setFlyPosition(null);
+            },
+        });
+        return position === null ? null : <Marker position={position} />;
+    };
+
     const submit = handleSubmit(async (data) => {
         try {
-            const rp = await guardarDonanteRequest(data);
+            console.log(data);
+            //const rp = await guardarDonanteRequest(data);
             reset();
             toastSucess();
         } catch (error) {
@@ -32,6 +83,19 @@ function FormDonante() {
             style: {
                 background: "#212121",
                 color: "white",
+            },
+        });
+    };
+
+    const LocationMarker = () => {
+        const map = useMapEvents({
+            click() {
+                map.locate();
+            },
+            locationfound(e) {
+                console.log(e.latlng.lat);
+                map.flyTo(e.latlng, map.getZoom());
+                setFlyPosition(e.latlng);
             },
         });
     };
@@ -92,13 +156,65 @@ function FormDonante() {
                                 <label className="block text-base text-white mt-2 font-semibold">
                                     Dirección
                                 </label>
-                                <textarea
-                                    className="w-full px-5 py-1 text-gray-900 bg-gray-200 rounded focus:outline-none focus:bg-gray-100 resize-none placeholder-slate-600"
-                                    placeholder="Escriba su lugar de residencia o de donde buscar"
-                                    {...register("direccion", {
-                                        required: true,
-                                    })}
-                                />
+                                <Button
+                                    variant="contained"
+                                    onClick={handleClickOpen}
+                                    className="px-4 py-1 mt-4 text-black font-thin tracking-wider bg-white hover:bg-slate-100 rounded"
+                                >
+                                    Abrir Mapa
+                                </Button>
+
+                                <Dialog
+                                    fullWidth={true}
+                                    maxWidth={"md"}
+                                    open={open}
+                                    onClose={handleClose}
+                                >
+                                    <DialogTitle
+                                        sx={{ m: 0, p: 2 }}
+                                        id="customized-dialog-title"
+                                    >
+                                        Seleccione la ubicación de su vivienda
+                                    </DialogTitle>
+                                    <IconButton
+                                        aria-label="inert"
+                                        onClick={handleClose}
+                                        sx={(theme) => ({
+                                            position: "absolute",
+                                            right: 8,
+                                            top: 8,
+                                            color: theme.palette.grey[500],
+                                        })}
+                                    ></IconButton>
+                                    <DialogContent dividers>
+                                        <div>
+                                            <MapContainer
+                                                center={positionLoad}
+                                                zoom={15}
+                                                scrollWheelZoom={true}
+                                                style={{
+                                                    height: 600,
+                                                    maxWidth: "100vh",
+                                                }}
+                                            >
+                                                <TileLayer
+                                                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                                />
+                                                {flyPosition === null ? (
+                                                    <LocationMarker />
+                                                ) : null}
+
+                                                <Markers />
+                                            </MapContainer>
+                                        </div>
+                                    </DialogContent>
+                                    <DialogActions>
+                                        <Button autoFocus onClick={handleClose}>
+                                            Cerrar
+                                        </Button>
+                                    </DialogActions>
+                                </Dialog>
 
                                 {errors.direccion && (
                                     <div className="text-red-500">
